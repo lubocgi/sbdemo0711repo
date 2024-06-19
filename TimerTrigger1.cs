844c7f905fb4d60b687103c724633c1d0efff38c
Luboslav Lisanik <luboslav.lisanik@cgi.com>
2024-06-19 10:39:45 +0200

    add timer fun TimerTrigger1

---
+++ TimerTrigger1.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class TimerTrigger1
+    {
+        private readonly ILogger _logger;
+
+        public TimerTrigger1(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TimerTrigger1>();
+        }
+
+        [Function("TimerTrigger1")]
+        public void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            
+            if(DateTime.Now.Minute % 2 == 1) {
+                throw new NotSupportedException("odd minute execution not supported");
+            }
+
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
